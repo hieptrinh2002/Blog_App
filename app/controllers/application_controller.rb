a3da5e2f74952bfe45ff2b51c3be853dd41ce6cf
Trịnh Hiệp <90686182+hieptrinh2002@users.noreply.github.com>
2024-04-28 14:18:45 +0700

    add search func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :set_notifications, if: :current_user
-    
+    before_action :set_query 
+
     private
     def set_notifications
       notifications = Notification.where(recipient: current_user).newest_first.limit(9)
       @unread = notifications.unread
       @read = notifications.read
     end
+
+    def set_query
+      @query = Post.ransack(params[:q])
+      @posts = @query.result(distinct: true)
+    end
+
   end
