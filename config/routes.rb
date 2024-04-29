ac987ee632b46945157179e449c6779d4197598a
Trịnh Hiệp <90686182+hieptrinh2002@users.noreply.github.com>
2024-04-30 01:14:18 +0700

    1cm

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,12 @@ Rails.application.routes.draw do
   end
 
   #get 'home', to: 'pages#home'
+
+
   get 'about', to: 'pages#about'
   get 'u/profile/:id', to: 'users#profile', as:"user" #{as dung de dat lai ten user_path(id: 1) #=> "/u/1"}
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
